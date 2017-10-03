fad0bec5c68ec59a0f445532dd5512bf24822b95
Ludovit Hajzer <ludovit.hajzer@mimacom.com>
2017-10-03 19:06:42 +0200

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {AppComponent} from './app.component';
 import {appRoutes} from './app.routes';
 import {LayoutComponent} from './layout/layout.component';
+import {PersonsEffects} from './persons/store/persons.effects';
 import {InMemoryService} from './services/in-memory.service';
 import {PartyService} from './services/party.service';
 import {PersonService} from './services/person.service';
+import {reducers} from './store/app.reducers';
 
 @NgModule({
 	declarations: [
@@ -22,7 +26,9 @@ import {PersonService} from './services/person.service';
 		HttpClientInMemoryWebApiModule.forRoot(
 			InMemoryService,
 			{delay: 500, dataEncapsulation: false}
-		)
+		),
+		StoreModule.forRoot(reducers),
+		EffectsModule.forRoot([PersonsEffects])
 	],
 	providers: [
 		PersonService,
