7b62f70d11584012a157a3cf700606612304349e
witcher_the <marek.vodicka.witcher@gmail.com>
2017-10-04 09:53:58 +0200

    implemented custom route serializer to get path params from route

--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,7 +1,30 @@
+import {
+	Params,
+	RouterStateSnapshot
+} from '@angular/router';
+import * as fromRouter from '@ngrx/router-store';
+import {RouterStateSerializer} from '@ngrx/router-store';
 import {ActionReducerMap} from '@ngrx/store';
 
+export interface RouterStateUrl {
+	url: string;
+	pathParams: Params;
+}
+
+export class CustomRouterStateSerializer implements RouterStateSerializer<RouterStateUrl> {
+
+	serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+		const {url} = routerState;
+		const pathParams = routerState.root.children[0].children[0].params;
+
+		return {url, pathParams};
+	}
+}
+
 export interface AppState {
+	routerReducer: fromRouter.RouterReducerState<RouterStateUrl>;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
+	routerReducer: fromRouter.routerReducer
 };
